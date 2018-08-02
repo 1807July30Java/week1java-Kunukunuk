bc627c9c0c890d78951d5a65bba2320c0d784a4c
Kun Huang <kkun.huang@gmail.com>
2018-08-02 09:02:29 -0400

    finished on question 4

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -139,8 +139,39 @@ public class EvaluationService {
 	 */
 	public int getScrabbleScore(String string) {
 		// TODO Write an implementation for this method declaration
+		string = string.toLowerCase();
+		int score = 0;
+
+		for (int i = 0; i < string.length(); i++) {
+			switch (string.charAt(i)) {
+				case 'a': case 'e': case 'i': case 'o': case 'u': case 'l': case 'n': case 'r': case 's': case 't':
+					score += 1;
+					break;
+				case 'd': case 'g':
+					score += 2;
+					break;
+				case 'b': case 'c': case 'm': case 'p':
+					score += 3;
+					break;
+				case 'f': case 'h': case 'v': case 'w': case 'y':
+					score += 4;
+					break;
+				case 'k':
+					score += 5;
+					break;
+				case 'j': case 'x':
+					score += 8;
+					break;
+				case 'q': case 'z':
+					score += 10;
+					break;
+				default:
+					score += 0;
+					break;
+			}
+		}
 		
-		return 0;
+		return score;
 	}
 
 	/**
@@ -176,7 +207,13 @@ public class EvaluationService {
 	 */
 	public String cleanPhoneNumber(String string) {
 		// TODO Write an implementation for this method declaration
-		return null;
+		String phoneNum = "";
+
+		for (int i =0; i < string.length(); i++) {
+
+		}
+		
+		return phoneNum;
 	}
 
 	/**
